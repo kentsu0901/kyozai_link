ab72119dc6e9c1a0fdccce8f445ea64699288e99
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-12 16:47:06 +0900

    Subjectの並び順を重複禁止にし、モデル単体テストを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_09_12_040326) do
+ActiveRecord::Schema[7.1].define(version: 2026_09_12_073251) do
   create_table "academic_years", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "year", null: false
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_09_12_040326) do
     t.datetime "updated_at", null: false
     t.integer "sort_order", null: false
     t.index ["name"], name: "index_subjects_on_name", unique: true
+    t.index ["sort_order"], name: "index_subjects_on_sort_order", unique: true
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
