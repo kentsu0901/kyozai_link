2483c29bfba70e06cd23bd6576f5d6067f7d95bd
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-13 10:35:40 +0900

    Courseの新規登録機能とフォーム画面を追加

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,28 @@ class CoursesController < ApplicationController
   def index
     @subjects = Subject.order(:sort_order)
   end
+
+  def new
+    @course = Course.new
+    @subjects = Subject.order(:sort_order)
+    @academic_years = AcademicYear.order(:year)
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @subjects = Subject.order(:sort_order)
+    @academic_years = AcademicYear.order(:year)
+
+    if @course.save
+      redirect_to new_course_path, notice: '授業を登録しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :grade, :subject_id, :academic_year_id)
+  end
 end
