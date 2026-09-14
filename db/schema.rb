cfd6b0e98a7a713035f37aa054e71508cb72a859
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-14 19:23:37 +0900

    Materialのenum化と教材一覧表示を調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_09_14_012159) do
+ActiveRecord::Schema[7.1].define(version: 2026_09_14_100058) do
   create_table "academic_years", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "year", null: false
     t.datetime "created_at", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_09_14_012159) do
   create_table "materials", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "publisher", null: false
-    t.string "material_type", null: false
+    t.integer "material_type", null: false
     t.integer "price", default: 0, null: false
     t.bigint "subject_id", null: false
     t.datetime "created_at", null: false
@@ -73,12 +73,12 @@ ActiveRecord::Schema[7.1].define(version: 2026_09_14_012159) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.string "role", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/factories/materials.rb
+++ spec/factories/materials.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :material do
-    name { "数学Ⅰ" }
-    publisher { "数研出版" }
-    material_type { "textbook" }
+    name { '数学Ⅰ' }
+    publisher { '数研出版' }
+    material_type { :textbook }
     price { 1000 }
 
     association :subject
