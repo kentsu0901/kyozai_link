8541c46aa89dd8948db531b448a23b8af28b430c
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-14 20:21:09 +0900

    Materialの登録・一覧・編集・削除機能を追加

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,8 +1,14 @@
 class MaterialsController < ApplicationController
-  before_action :set_subjects, only: [:new, :create]
+  before_action :set_subjects, only: [:new, :create, :edit, :update]
+  before_action :set_material, only: [:edit, :update, :destroy]
+  before_action :authorize_material!, only: [:edit, :destroy]
 
   def index
-    @subjects = Subject.order(:sort_order)
+    @subjects = if current_user.system_admin?
+                  Subject.order(:sort_order)
+                else
+                  current_user.subjects.order(:sort_order)
+                end
   end
 
   def new
@@ -12,6 +18,11 @@ class MaterialsController < ApplicationController
   def create
     @material = Material.new(material_params)
 
+    unless authorized_subject?(@material.subject_id)
+      redirect_to materials_path, alert: '担当外の教科の教材は登録できません'
+      return
+    end
+
     if @material.save
       redirect_to new_material_path, notice: '教材を登録しました'
     else
@@ -19,23 +30,52 @@ class MaterialsController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
 
-  def set_subjects
-    if current_user.role == 'system_admin'
-      @subjects = Subject.order(:sort_order)
+  def update
+    unless authorized_subject?(material_params[:subject_id].to_i)
+      redirect_to materials_path, alert: '担当外の教科には変更できません'
+      return
+    end
+
+    if @material.update(material_params)
+      redirect_to materials_path, notice: '教材情報を更新しました'
     else
-      @subjects = current_user.subjects.order(:sort_order)
+      render :edit, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @material.destroy
+    redirect_to materials_path, notice: '教材を削除しました'
+  end
+
+  private
+
+  def set_material
+    @material = Material.find(params[:id])
+  end
+
+  def set_subjects
+    @subjects = if current_user.system_admin?
+                  Subject.order(:sort_order)
+                else
+                  current_user.subjects.order(:sort_order)
+                end
+  end
+
+  def authorize_material!
+    return if authorized_subject?(@material.subject_id)
+
+    redirect_to materials_path, alert: 'この教材を操作する権限がありません'
+  end
+
+  def authorized_subject?(subject_id)
+    current_user.system_admin? || current_user.subject_ids.include?(subject_id)
+  end
+
   def material_params
-    params.require(:material).permit(
-      :name,
-      :publisher,
-      :material_type,
-      :price,
-      :subject_id
-    )
+    params.require(:material).permit(:name, :publisher, :material_type, :price, :subject_id)
   end
 end
