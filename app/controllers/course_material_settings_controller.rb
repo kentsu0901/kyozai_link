25131805f3ffaef010edadb4579349b3154f0724
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-16 19:11:17 +0900

    授業と教材の紐付け機能を実装

--- app/controllers/course_material_settings_controller.rb
+++ app/controllers/course_material_settings_controller.rb
@@ -4,4 +4,25 @@ class CourseMaterialSettingsController < ApplicationController
     @courses = @subject.courses
     @materials = @subject.materials
   end
+
+  def update
+    subject = Subject.find(params[:subject_id])
+    courses = subject.courses
+
+    if params[:course_materials].nil?
+      courses.each do |course|
+        course.material_ids = []
+      end
+    else
+      courses.each do |course|
+        course.material_ids = if params[:course_materials][course.id.to_s].nil?
+                                []
+                              else
+                                params[:course_materials][course.id.to_s]
+                              end
+      end
+    end
+
+    redirect_to course_materials_path
+  end
 end
