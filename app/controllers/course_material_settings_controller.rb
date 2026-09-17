2484d724871a0e5bd6cfd769517d6dbe9e20c8dd
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-17 11:34:25 +0900

    教材設定に権限制御を追加

--- app/controllers/course_material_settings_controller.rb
+++ app/controllers/course_material_settings_controller.rb
@@ -1,13 +1,13 @@
 class CourseMaterialSettingsController < ApplicationController
+  before_action :set_subject, only: [:edit, :update]
+  before_action :authorized_material_setting!, only: [:edit, :update]
   def edit
-    @subject = Subject.find(params[:subject_id])
     @courses = @subject.courses
     @materials = @subject.materials
   end
 
   def update
-    subject = Subject.find(params[:subject_id])
-    courses = subject.courses
+    courses = @subject.courses
 
     if params[:course_materials].nil?
       courses.each do |course|
@@ -24,5 +24,21 @@ class CourseMaterialSettingsController < ApplicationController
     end
 
     redirect_to course_materials_path
+  end
+
+  private
+
+  def authorized_material_setting!
+    return if authorized_subject?(@subject.id)
+
+    redirect_to course_materials_path, alert: 'この教材登録を操作する権限がありません'
+  end
+
+  def authorized_subject?(subject_id)
+    current_user.system_admin? || current_user.subject_ids.include?(subject_id)
+  end
+
+  def set_subject
+    @subject = Subject.find(params[:subject_id])
   end
 end
